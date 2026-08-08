77addfe2dba2bbf374c323438110fbd0e874d3f0
dylanpincay <pincaysalazarjeanpier@gmail.com>
2026-08-08 16:23:42 -0500

    Update Main.java

--- src/main/java/com/espol/proyectoestruturadatos/model/game/Main.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/game/Main.java
@@ -11,6 +11,6 @@ package com.espol.proyectoestruturadatos.model.game;
 public class Main {
     public static void main(String[] args){
         System.out.println("HOLA HELEN");
-        
+        // AMIGOS
     }
 }
