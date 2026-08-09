605176eab1c80f831d8d6d5ea11b284cf959b33d
dylanpincay <pincaysalazarjeanpier@gmail.com>
2026-08-09 10:51:50 -0500

    Update Board.java

--- src/main/java/com/espol/proyectoestruturadatos/model/game/Board.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/game/Board.java
@@ -4,6 +4,8 @@
  */
 package com.espol.proyectoestruturadatos.model.game;
 
+import java.util.LinkedList;
+
 /*
  * @author Dylan Jeanpier Pincay Salazar
  * @author Gabriel Cevallos
@@ -11,15 +13,31 @@ package com.espol.proyectoestruturadatos.model.game;
  */
 
 public class Board {
-    private final Integer rows = 3; // filas de tablero.
-    private final Integer columns = 3; // columnas del tablero.
-    public Box[] boxes; // Matrix 3 x 3, osea el contenido del tablero.
-    // public List<Subscriber> suscribers; 
+    // private final Integer rows = 3; // filas de tablero.
+    // private final Integer columns = 3; // columnas del tablero.
+    public Box[] boxes = new Box[9]; // Matrix 3 x 3, osea el contenido del tablero.
+    public List<Subscriber> suscribers;  ///////////////////////////////////////////
     private Symbol winner; // Conocer el ganador
     private int lastMovement; // Ultima jugada que se realizo para llegar a ese estado.
     public boolean hasEnded; // Verifica si la partida a terminado.
-    // public boolean choosen;
+    public boolean choosen; ///////////////////////////////////////////////////////
    
+    public Board(){
+        hasEnded = false;
+        suscribers = new LinkedList<>();
+        
+        for(int i = 0; i < boxes.length; i++){
+            boxes[i] = new Box();
+        }
+    }
+    
+    public Board(Board board){
+        this(); // llamamos el contructor anterior
+        for(int i = 0; i < boxes.length; i++ ){
+            boxes[i] = new Box(board.boxes[i]);
+        }
+    }
+    
     
     
 }
