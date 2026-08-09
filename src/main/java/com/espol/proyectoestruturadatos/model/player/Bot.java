4f8b1a4fb5bdb3c9cc53e7dfa1968f476b99f3ba
Helen Romina <romcm20@gmail.com>
2026-08-09 14:41:50 -0500

    iterators y player

--- src/main/java/com/espol/proyectoestruturadatos/model/player/Bot.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/player/Bot.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.espol.proyectoestruturadatos.model.player;
 
-/**
- *
- * @author Helen
- */
-public class Bot {
-    
+import com.espol.proyectoestruturadatos.model.board.Symbol;
+
+public class Bot extends Player {
+
+    public Bot() {
+        super();
+    }
+
+    public Bot(Symbol symbol) {
+        super(symbol);
+    }
 }
--- src/main/java/com/espol/proyectoestruturadatos/model/player/Player.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/player/Player.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.espol.proyectoestruturadatos.model.player;
 
+package com.espol.proyectoestruturadatos.model.player;
+import com.espol.proyectoestruturadatos.model.board.Symbol;
 /**
  *
  * @author Helen
  */
-public class Player {
-    
+public abstract class Player {
+
+    protected Symbol symbol;
+    protected int wins;
+
+    public Player() {
+        this.wins = 0;
+    }
+
+    public Player(Symbol symbol) {
+        this.symbol = symbol;
+        this.wins = 0;
+    }
+
+    public Symbol getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(Symbol symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public void addWin() {
+        wins++;
+    }
+
+    public String getColor() {
+        return symbol.getColor();
+    }
+
+    public void setColor(String color) {
+        symbol.setColor(color);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName()
+                + "(" + symbol + ")";
+    }
 }
