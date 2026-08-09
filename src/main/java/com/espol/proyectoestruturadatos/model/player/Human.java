055ac32cfe28c9ed9dd02a4c97e779b48f8a576d
Helen Romina <romcm20@gmail.com>
2026-08-09 14:46:41 -0500

    Update Human.java

--- src/main/java/com/espol/proyectoestruturadatos/model/player/Human.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/player/Human.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.espol.proyectoestruturadatos.model.player;
 
-/**
- *
- * @author Helen
- */
-public class Human {
-    
+import com.espol.proyectoestruturadatos.model.board.Symbol;
+
+public class Human extends Player {
+
+    public Human() {
+        super();
+    }
+
+    public Human(Symbol symbol) {
+        super(symbol);
+    }
 }
