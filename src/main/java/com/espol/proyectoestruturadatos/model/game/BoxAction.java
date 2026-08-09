e1a7e143c513dadab71b7fd82bf372a5e56b08b9
dylanpincay <pincaysalazarjeanpier@gmail.com>
2026-08-09 11:01:35 -0500

    Create BoxAction.java

---
+++ src/main/java/com/espol/proyectoestruturadatos/model/game/BoxAction.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectoestruturadatos.model.game;
+
+/*
+ * @author Dylan Jeanpier Pincay Salazar
+ * @author Gabriel Cevallos
+ * @author Helen Cruz
+ */
+
+public interface BoxAction {
+    void execute(Box box); // Permite generar una accion 
+}
