4e10373662fc3edf8219ec6368e106ead3f7a9b9
dylanpincay <pincaysalazarjeanpier@gmail.com>
2026-08-09 11:32:33 -0500

    Create Symbol.java

---
+++ src/main/java/com/espol/proyectoestruturadatos/model/game/Symbol.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.proyectoestruturadatos.model.game;
+
+// A CARGO: JEAN PINCAY 
+/*
+ * @author Dylan Jeanpier Pincay Salazar
+ * @author Gabriel Cevallos
+ * @author Helen Cruz
+ */
+
+public class Symbol {
+    private Character character;
+    private String color;
+    
+    public Symbol(Character ){
+        
+    }
+}
