379ae1bc4654547d311f16a8a7cdc6c789554ac3
dylanpincay <pincaysalazarjeanpier@gmail.com>
2026-08-09 10:29:29 -0500

    Update Board.java

--- src/main/java/com/espol/proyectoestruturadatos/model/game/Board.java
+++ src/main/java/com/espol/proyectoestruturadatos/model/game/Board.java
@@ -11,5 +11,15 @@ package com.espol.proyectoestruturadatos.model.game;
  */
 
 public class Board {
+    private final Integer rows = 3; // filas de tablero.
+    private final Integer columns = 3; // columnas del tablero.
+    public Box[] boxes; // Matrix 3 x 3, osea el contenido del tablero.
+    // public List<Subscriber> suscribers; 
+    private Symbol winner; // Conocer el ganador
+    private int lastMovement; // Ultima jugada que se realizo para llegar a ese estado.
+    public boolean hasEnded; // Verifica si la partida a terminado.
+    // public boolean choosen;
+   
+    
     
 }
